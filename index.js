14868771ee2a2d0e8b69bb8b25ee99083d51f9d3
flftfqwxf <flftfqwxf@qq.com>
2015-01-05 16:52:31 +0800

    update reg and add quote mark
    update reg and add quote mark

--- index.js
+++ index.js
@@ -32,7 +32,10 @@ module.exports = function(options) {
 
     var incoming = file.contents.toString();
 
-    var outgoing = incoming.replace(/url(\([\s]*.*[\s]*\))/g, function (str, dirtyUrl) {
+    var outgoing = incoming.replace(/url(\([\s]*(["'])[^\2\s]*\2\))|url(\([\s]*[^'"\s]*\))/g, function (str, dirtyUrl) {
+        if (dirtyUrl===undefined) {
+            dirtyUrl=arguments[3];
+        }
       var url = dirtyUrl.trim().replace(/^\(/g,'').replace(/\)$/g,'').replace(/'|"/g, '');
       var replaceWithStr = null;
       if (url.indexOf("base64,") > -1 || url.indexOf("http://") > -1 ) {
@@ -49,7 +52,8 @@ module.exports = function(options) {
 
         try {
           var idata = fs.readFileSync(imagePath);
-          replaceWithStr = 'url(' + url + "?v=" + md5ify(idata) + ')';
+          //must add quote mark ,because file name or dir has ")"
+          replaceWithStr = 'url("' + url + "?v=" + md5ify(idata) + '")';
         } catch(err) {
           replaceWithStr = str;
           console.dir(file);
